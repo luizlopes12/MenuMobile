d202e8bb2d7cd01936a7a49363c442150a7e5963
Luiz Lopes <luizlopes12@outlook.com>
2021-05-01 12:40:37 -0300

    menu terminado

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu')
+const btnMenuOpen = document.querySelector('.btn-menu-open');
+const btnMenuClose = document.querySelector('.btn-menu-close');
+
+btnMenuOpen.addEventListener('click', function(){
+    menu.classList.add('menu_open')
+})
+btnMenuClose.addEventListener('click', function(){
+    menu.classList.remove('menu_open')
+})
