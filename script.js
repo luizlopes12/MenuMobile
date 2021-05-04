309800e7d59f6ffafd4a7fdaf25596106e0bdb68
Luiz Lopes <luizlopes12@outlook.com>
2021-05-03 23:01:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const btnMenuOpen = document.querySelector('.btn-menu-open');
 const btnMenuClose = document.querySelector('.btn-menu-close');
 
 btnMenuOpen.addEventListener('click', function(){
-    menu.classList.add('menu_open')
+    menu.classList.add('menu_open');
+
 })
 btnMenuClose.addEventListener('click', function(){
     menu.classList.remove('menu_open')
